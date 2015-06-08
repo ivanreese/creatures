bf3a67f1172599c3bd3936326ee80991a235fe45
Ivan Reese <ivan@cdiginc.com>
2015-06-08 17:51:46 +0200

    Basic SVG example

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
-(function() {
-  return console.log("TEST2");
-});
+var circle, svg, svgns;
+
+svgns = "http://www.w3.org/2000/svg";
+
+svg = document.querySelector("svg");
+
+circle = document.createElementNS(svgns, "circle");
+
+circle.setAttribute("cx", "100");
+
+circle.setAttribute("cy", "100");
+
+circle.setAttribute("r", "100");
+
+svg.appendChild(circle);
--- source/script.coffee
+++ source/script.coffee
@@ -1,2 +1,8 @@
-()->
-  console.log "TEST2"
+svgns = "http://www.w3.org/2000/svg"
+svg = document.querySelector "svg"
+
+circle = document.createElementNS svgns, "circle"
+circle.setAttribute("cx", "100")
+circle.setAttribute("cy", "100")
+circle.setAttribute("r", "100")
+svg.appendChild(circle)
